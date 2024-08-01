5a2754230a937126664f05a7578da29cdb26a67b
hg231049 <dmstj3156@naver.com>
2024-08-01 18:39:43 +0900

    les fullpage적용

--- js/style.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-   $('#header').load('./layout/header.html');
-   $('#footer').load('./layout/footer.html');
-});
---
+++ les_file/js/style.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+   $('#header').load('./les_file/layout/header.html');
+   $('#footer').load('./les_file/layout/footer.html');
+});
