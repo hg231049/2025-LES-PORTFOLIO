ef7287a1b2d031387b55b0b63756e210f0e053de
hg231049 <dmstj3156@naver.com>
2024-09-05 17:28:54 +0900

    les 스크립트 추가

--- dles/js/style.js
+++ dles/js/style.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
-   $('#header').load('/dles/layout/header.html');
-   $('#main').load('/main.html');
-   $('#footer').load('/dles/layout/footer.html');
+   
+   
+  
    
 });
